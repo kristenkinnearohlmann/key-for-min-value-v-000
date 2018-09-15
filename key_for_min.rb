8cf6b3a1487d581e64b3481b05a1373623d9c951
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-15 19:44:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,11 @@ def key_for_min_value(name_hash)
     low_key = ""
     name_hash.each do |key, value|
         if low_value == -1
+          puts "cond 1"
           low_value = value
           low_key = key
         elsif low_value > value
+          puts "cond 2"
           low_value = value
           low_key = key
         end
